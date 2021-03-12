79a4b7bb76303d64db55d8e8e7f42b5dfeb993b8
Nicolas Lopez <nlopez97f@gmail.com>
2021-03-11 22:10:39 -0500

    button action

--- src/GifApp.js
+++ src/GifApp.js
@@ -1,10 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export const GifApp = () => {
+
+    // const categories = ['Correr', 'Nadar', 'Bucear'];
+    const [categories, setCategories] = useState(['Correr', 'Nadar', 'Bucear']);
+
+    const handleAdd = () => {
+        setCategories( [...categories, 'Caminata'] );
+    }
+    
     return (
         <div>
             <h2>GifApplication</h2>
             <hr />
+
+            <button onClick= { handleAdd }> Agregar </button>
+
+            <ol>
+                { 
+                    categories.map( (categoria, i) => {
+                        return <li key={ categoria }> { categoria } </li>;
+                    } )
+                }
+            </ol>
+
         </div>
     )
 }
