c35bced0df1849debb4957afdc80c5e9b7f38be3
Nicolas Lopez <nlopez97f@gmail.com>
2021-04-10 00:08:06 -0500

    gifgriditem tests

---
+++ src/tests/components/GifGridItem.test.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { shallow } from "enzyme"
+import { GifGridItem } from "../../components/GifGridItem"
+
+describe('Pruebas en <GifGridItem />', () => {
+
+    const title = 'Un título';
+    const url = 'https://localhost/algo.jpg';
+    const wrapper = shallow( <GifGridItem title={ title } url={ url } /> )
+
+
+    test('Mostrar componente correctamente ', () => {
+
+        expect( wrapper ).toMatchSnapshot();
+
+    })
+
+    test('Debe de tener un parrafo con el title', () => {
+
+        const p = wrapper.find('p');
+        expect( p.text().trim() ).toBe( title );
+
+    })
+
+    test('debe de tener la imagen igual al url y alt de los props ', () => {
+        
+        const img = wrapper.find('img');
+        expect( img.props().src ).toBe( url );
+        expect( img.props().alt ).toBe( title );
+
+    })
+    
+    test('debe de tener animate__fadeIn', () => {
+
+        const div = wrapper.find('div');
+
+        const className = div.props().className;
+
+        expect( className.includes('animate__fadeIn') ).toBe( true );
+
+    })
+    
+})
